d5ba5587df37a5770c4f905855d57bef8c6807ff
Hwl321 <166300013+Hwl321@users.noreply.github.com>
2025-02-16 23:20:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,11 @@ let mainImage = document.getElementById("mainImage");
 let clickCount = 0;  // 记录点击 No 的次数
 
 // No 按钮的文字变化
-const noTexts = 
-[
-    "??bb你认真的吗…", 
-    "宝贝要不再想想？", 
-    "宝宝不许选这个！ ", 
-    "小宝我会很伤心…", 
+const noTexts = [
+    "??bb你认真的吗…",
+    "宝贝要不再想想？",
+    "宝宝不许选这个！ ",
+    "小宝我会很伤心…",
     "绝对不行！！！"
 ];
 const container = document.querySelector('.container');
@@ -22,23 +21,23 @@ function getRandomPosition(element) {
     if (!containerRect) {
         containerRect = container.getBoundingClientRect();
     }
- 
+
     // 获取按钮自身的尺寸
     const elementRect = element.getBoundingClientRect();
- 
+
     // 计算容器内可移动的最大范围
     const maxX = containerRect.width - elementRect.width;
     const maxY = containerRect.height - elementRect.height;
- 
+
     // 生成随机位置
     const randomX = Math.floor(Math.random() * maxX);
     const randomY = Math.floor(Math.random() * maxY);
- 
+
     return { top: randomY + 'px', left: randomX + 'px' };
 }
 
 // No 按钮点击事件
-noButton.addEventListener("click", function() {
+noButton.addEventListener("click", function () {
     clickCount++;
 
     // 让 Yes 变大，每次放大 2 倍
@@ -60,30 +59,21 @@ noButton.addEventListener("click", function() {
     if (clickCount <= 5) {
         noButton.innerText = noTexts[clickCount - 1];
     }
-    let imageSrc;
-    switch (clickCount) {
-        case 1:
-            imageSrc = "images/shocked.gif"; // 震惊
-            break;
-        case 2:
-            imageSrc = "images/think.gif";   // 思考
-            break;
-        case 3:
-            imageSrc = "images/angry.gif";   // 生气
-            break;
-        case 4:
-        case 5:
-        default:
-            imageSrc = "images/crying.gif";  // 哭（之后一直是哭）
-            break;
-    }
+
+    // 图片变化（前 5 次变化）
+    if (clickCount == 1) mainImage.src = "images/shocked.gif"; // 震惊
+    if (clickCount == 2) mainImage.src = "images/think.gif";   // 思考
+    if (clickCount == 3) mainImage.src = "images/angry.gif";   // 生气
+    if (clickCount == 4) mainImage.src = "images/crying.gif";  // 哭
+    if (clickCount >= 5) mainImage.src = "images/crying.gif";  // 之后一直是哭
+
 });
 
 // Yes 按钮点击后，进入表白成功页面
-yesButton.addEventListener("click", function() {
+yesButton.addEventListener("click", function () {
     document.body.innerHTML = `
         <div class="yes-screen">
-            <h1 class="yes-text">!!!超超超超超超级喜欢你!!!</h1>
+            <h1 class="yes-text">!!!喜欢你!!!!</h1>
             <img src="images/hug.gif" alt="拥抱" class="yes-image">
         </div>
     `;
